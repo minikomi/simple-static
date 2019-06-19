e82813f2004d4fa28c8c682bf6ca7432fa4d2e28
Adam Moore <nerdfunk@gmail.com>
2019-06-19 11:55:29 +0900

    simplify getting nested deps

--- src/simple_static/components/helper/watch_and_run.clj
+++ src/simple_static/components/helper/watch_and_run.clj
@@ -31,65 +31,21 @@
                 :template pprint/pprint}}
               ))
 
-
-(defn get-dep-graph [src-paths]
-  (let [src-files
-        (apply set/union
-               (map (comp #(ns-find/find-clojure-sources-in-dir %)
-                          io/file)
-                    src-paths))
-        tracker (ns-file/add-files {} src-files)
-        dep-graph (tracker ::ns-track/deps)]
-    dep-graph))
-
 (defn get-watched-nested-deps []
  (let [tracked-src (:tracked-src env ["src" "data"])
        all-dependencies (:dependencies (get-dep-graph tracked-src))
        watched-ns-syms (set (map :ns (vals @watched)))
        ns-names (set (ns-find/find-namespaces (map io/file tracked-src)))
        part-of-project? (partial contains? ns-names)]
-   (loop [roots watched-ns-syms
-          stack []
-          current-root nil
-          current #{}
-          acc []]
-     (cond
-       ;; finished
-       (empty? roots)
-       (conj acc [current-root (into current stack)])
-       ;; stack is empty - start new
-       (empty? stack)
-       (recur
-        (rest roots)
-        [(first roots)]
-        (first roots)
-        #{(first roots)}
-        (if current-root
-          (conj acc [current-root (into current stack)])
-          acc))
-       :else
-       (let [deps (filter part-of-project?
-                          (get all-dependencies (peek stack)))]
-         (recur
-          roots
-          (into (pop stack) deps)
-          current-root
-          (into current deps)
-          acc))))))
-
-(defn get-dep-tree []
-  (let [ds (get-dep-graph)
-        edges (for [[d ds2] (:dependents ds)
-                   d2 ds2]
-               [d d2])
-        roots (set/difference (set (keys (:dependencies ds)))
-                              (set (keys (:dependents ds))))
-        subtrees (reduce
-                  (fn [acc [c p]]
-                           (update acc p assoc c (get acc c {})))
-                         {}
-                         edges)]
-    (select-keys subtrees roots)))
+   (for [ns-sym watched-ns-syms
+         :let [nested-local-deps
+               (tree-seq
+                identity
+                #(filter
+                  part-of-project?
+                  (get all-dependencies %))
+                ns-sym)]]
+     [ns-sym (set nested-local-deps)])))
 
 (defn ns-file-name
   "Copied from clojure.tools.namespace.move because it's private there."
@@ -135,8 +91,6 @@ namespace name that corresponds with the path name"
   :start
   (hawk/watch! [{:paths   (into
                            (:tracked-src env ["src" "data"]))
-                 :handler watch-handler}]
-               )
+                 :handler watch-handler}])
   :stop
-  (hawk/stop! watch-and-run)
-  )
+  (hawk/stop! watch-and-run))
